87a13b2245fb67258b8bce793ae243e41dd32921
frvncrz <cruzfrvn@icloud.com>
2025-02-08 11:25:38 -0500

    started creating neccesary files, but didn't realize they were created outside of repo folder. Added files to repo folder.

---
+++ etch.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("container");
+  container.style.display = "gtrid";
+  container.style.gridTemplateColumns = "repeat(16, 1fr)";
+  container.style.gridTemplateRows = "repeat(16, 1fr)";
+
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.style.border = "1px solid #414559";
+    container.appendChild(cell);
+  }
+});
